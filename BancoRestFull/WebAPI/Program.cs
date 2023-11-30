ee4edd2d82228432134268034b93d85d94d0d982
Maxruizdz <maxruizdz@hotmail.com>
2023-11-30 00:00:08 -0300

    Configurando la capa Application 2.0

--- BancoRestFull/WebAPI/Program.cs
+++ BancoRestFull/WebAPI/Program.cs
@@ -1,12 +1,14 @@
+using Application;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Add services to the container.by
+builder.Services.AddApplicationLayer();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
